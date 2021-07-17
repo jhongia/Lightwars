87794bb44699419a66fcbf23ca1fadaeb3979bde
Jhon <jhonjairo0897@hotmail.com>
2021-07-16 20:23:23 -0400

    Added Hashing Passwords.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { urlencoded } = require('body-parser');
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const dotenv = require('dotenv').config();
 const passport = require('passport');
 
@@ -24,7 +24,6 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
 const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
@@ -47,7 +46,7 @@ app.get('/logout', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
@@ -62,7 +61,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, foundUser) => {
         if (!foundUser) {
