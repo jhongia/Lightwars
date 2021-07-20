777d4164a3eed41932ffbc9938105783941327cc
Jhon <jhonjairo0897@hotmail.com>
2021-07-19 21:43:55 -0400

    Update Google

--- app.js
+++ app.js
@@ -67,10 +67,7 @@ passport.use(new GoogleStrategy({
     },
     function(accessToken, refreshToken, profile, cb) {
         console.log(profile);
-        User.findOrCreate({
-            googleId: profile.id,
-            username: profile.emails[0].value
-        }, (err, user) => {
+        User.findOrCreate({googleId: profile.id}, (err, user) =>{
             return cb(err, user);
         });
     }
